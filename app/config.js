2afa9f64137561e218ec67864bb5c992bffee1be
Kyle Baker <kylebaker@Kyles-MBP.lan>
2021-07-21 13:54:32 -0400

    Add styling and other refactoring
    Add styling and refactoring of login forms

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 let apiUrl
 const apiUrls = {
   production: 'https://tic-tac-toe-api-production.herokuapp.com',
-  development: 'https://tic-tac-toe-api-development.herokuapp.com',
-};
+  development: 'https://tic-tac-toe-api-development.herokuapp.com'
+}
 
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
--- app/ui.js
+++ app/ui.js
@@ -3,43 +3,45 @@ const store = require('./store')
 const onSignUpSuccess = function (response) {
   console.log('in then')
   $('#message').text(`Thank you for signing up ${response.user.email}`)
-  $('#sign-up').trigger('reset')
+  $('#sign-up1').trigger('reset')
 }
 
 const onSignUpFailure = function (response) {
   console.log('in catch')
   $('#message').text('Sign up failed')
-  $('#sign-up').trigger('reset')
+  $('#sign-up1').trigger('reset')
 }
 const onSignInSuccess = function (response) {
   console.log('in then')
   $('#message').text(`Now signed in as ${response.user.email}`)
   store.token = response.user.token
-  $('#sign-in').trigger('reset')
-  $('#sign-in').hide()
-  $('#sign-up').hide()
-  $('#sign-out').show()
-  $('#new-game').show()
+  $('#sign-in1').trigger('reset')
+  $('#sign-in1').hide()
+  $('#sign-up1').hide()
+  $('#sign-out1').show()
+  $('.new-game1').show()
   $('#sign-in-title').hide()
   $('#sign-up-title').hide()
   // $('#change-password').show()
-
 }
 
 const onSignInFailure = function (response) {
   console.log('in catch')
   $('#message').text('Sign in failed, please enter a valid Email and password.')
-  $('#sign-in').trigger('reset')
+  $('#sign-in1').trigger('reset')
 }
 
 const onSignOutSuccess = function (response) {
   console.log('in then')
   $('#message').text('Signed Out')
-  $('#sign-in').show()
-  $('#sign-up').show()
-  $('#sign-out').hide()
+  $('#sign-in1').show()
+  $('#sign-up1').show()
+  $('#sign-out1').hide()
   $('.container').hide()
-  $('#new-game').hide()
+  $('.new-game1').hide()
+  $('#button1').show()
+  $('#message').delay(3000).hide(500)
+  // $('.status').delay(5000).fadeOut(500)
 }
 
 const onSignOutFailure = function (response) {
@@ -49,6 +51,20 @@ const onSignOutFailure = function (response) {
 
 const onNewGameSuccess = function (response) {
   store.id = response.game._id
+  $('#sign-up1').hide()
+  // $('.form-modal').hide()
+  $('#button1').hide('slow')
+  $('.form-modal').trigger('exit')
+  $('#sign-out1').show()
+}
+const onNewGameFailure = function () {
+  ('#message').text('Sorry, something went wrong')
+}
+const onCellClickSuccess = function () {
+  console.log('in then')
+}
+const onCellClickFailure = function () {
+  '#message'.text('Sorry, something went wrong')
 }
 module.exports = {
   onSignUpSuccess,
@@ -57,5 +73,8 @@ module.exports = {
   onSignInFailure,
   onSignOutSuccess,
   onSignOutFailure,
-  onNewGameSuccess
+  onNewGameSuccess,
+  onNewGameFailure,
+  onCellClickSuccess,
+  onCellClickFailure
 }
